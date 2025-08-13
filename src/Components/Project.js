f2d1113314f5d48125cca05ef9e205b2de182654
Chen <chenzalman96@gmail.com>
2025-08-13 18:45:52 +0300

    Fixed the error Netlify requested to fix

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -4,7 +4,7 @@ export default function Project({props}) {
   return (
     <div>
       <h4>{props.title}</h4>
-      <img height={'300px'} width={'500px'}  src={props.image} alt="No cover photo provided" />
+      <img height={'300px'} width={'500px'}  src={props.image} />
       <p>{props.description}</p>
     </div>
   )
